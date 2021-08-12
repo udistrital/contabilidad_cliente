9c9c64a451779522e391a89bfdc4383a90e72c24
Jeisson P <jeisson005@hotmail.com>
2021-08-11 23:18:08 -0500

    fix: Ajuste identación #116

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,27 +4,25 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
 export const environment = {
-
-    production: false,
-    NUXEO: {
-        PATH: 'https://documental.udistrital.edu.co/nuxeo/',
-    },
-    CLIENTE_PRESUPUESTO: 'https://pruebaspresupuesto.portaloas.udistrital.edu.co/pages',
-    CLIENTE_CONTABILIDAD: '/pages/comprobantes',
-    WSO2_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/',
-    PLAN_CUENTAS_MONGO_SERVICE: 'http://pruebasapi.intranetoas.udistrital.edu.co:8203/v1/',
-    CUENTAS_CONTABLES_SERVICE: 'http://pruebasapi.intranetoas.udistrital.edu.co:8209/v1/',
-    CONFIGURACION_SERVICE: 'http://pruebasapi.intranetoas.udistrital.edu.co:8086/v1/',
-    CONF_MENU_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1/menu_opcion_padre/ArbolMenus/',
-    TOKEN: {
-        AUTORIZATION_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize',
-        CLIENTE_ID: 'e36v1MPQk2jbz9KM4SmKhk8Cyw0a',
-        RESPONSE_TYPE: 'id_token token',
-        SCOPE: 'openid email role',
-        REDIRECT_URL: 'http://localhost:4200/',
-        SIGN_OUT_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oidc/logout',
-        SIGN_OUT_REDIRECT_URL: 'http://localhost:4200/',
-    },
-
+  production: false,
+  NUXEO: {
+    PATH: 'https://documental.udistrital.edu.co/nuxeo/',
+  },
+  CLIENTE_PRESUPUESTO: 'https://pruebaspresupuesto.portaloas.udistrital.edu.co/pages',
+  CLIENTE_CONTABILIDAD: '/pages/comprobantes',
+  WSO2_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/',
+  PLAN_CUENTAS_MONGO_SERVICE: 'http://pruebasapi.intranetoas.udistrital.edu.co:8203/v1/',
+  CUENTAS_CONTABLES_SERVICE: 'http://pruebasapi.intranetoas.udistrital.edu.co:8209/v1/',
+  CONFIGURACION_SERVICE: 'http://pruebasapi.intranetoas.udistrital.edu.co:8086/v1/',
+  CONF_MENU_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1/menu_opcion_padre/ArbolMenus/',
+  TOKEN: {
+    AUTORIZATION_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize',
+    CLIENTE_ID: 'e36v1MPQk2jbz9KM4SmKhk8Cyw0a',
+    RESPONSE_TYPE: 'id_token token',
+    SCOPE: 'openid email role',
+    REDIRECT_URL: 'http://localhost:4200/',
+    SIGN_OUT_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oidc/logout',
+    SIGN_OUT_REDIRECT_URL: 'http://localhost:4200/',
+  },
 
 };
